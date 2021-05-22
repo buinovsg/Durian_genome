0822000b64b9f2fbf88865fed11e1263808e8471
buinovsg <43320908+buinovsg@users.noreply.github.com>
2021-05-22 15:09:28 +0200

    Add files via upload

---
+++ 07_Extra_Analyses_busco_original_run.sh
@@ -0,0 +1,15 @@
+#! /bin/bash -l
+#SBATCH -A g2021012
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 24:00:00
+#SBATCH -J busco_original_durian
+
+module load bioinfo-tools
+module load BUSCO/3.0.2b
+
+source $AUGUSTUS_CONFIG_COPY
+
+python /sw/bioinfo/BUSCO/3.0.2b/rackham/bin/run_BUSCO.py -i /home/grbui/DurianProject/data/Paper5_scaffold06/scaffold_6.fasta \
+ -o Durian_BUSCO -l $BUSCO_LINEAGE_SETS/embryophyta_odb9 -m geno
