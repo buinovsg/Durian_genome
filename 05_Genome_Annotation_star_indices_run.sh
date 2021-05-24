3c59559659845284078f00c86e9bdf6b181d7d7a
buinovsg <43320908+buinovsg@users.noreply.github.com>
2021-05-24 16:18:17 +0200

    Update 05_Genome_Annotation_star_indices_run.sh

--- 05_Genome_Annotation_star_indices_run.sh
+++ 05_Genome_Annotation_star_indices_run.sh
@@ -6,7 +6,6 @@
 #SBATCH -t 01:00:00
 #SBATCH -J durian_star_indices
 
-
 module load bioinfo-tools
 module load star
 
@@ -15,8 +14,3 @@ STAR --runThreadN 8 \
  --genomeDir /home/grbui/DurianProject/analyses/05_genome_annotation/STAR_RNAseq_align/genome_indices \
  --genomeFastaFiles /home/grbui/DurianProject/analyses/03_genome_correction/repeat_masking/durian.contig.masked.fasta \
  --genomeSAindexNbases 12
-#https://hbctraining.github.io/Intro-to-rnaseq-hpc-O2/lessons/03_alignment.html
-
-#https://physiology.med.cornell.edu/faculty/skrabanek/lab/angsd/lecture_notes/STARmanual.pdf
-
-#https://bioinformatics.stackexchange.com/questions/4364/can-i-run-star-without-an-annotation-file
